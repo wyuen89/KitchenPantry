1a17aa272718d9d62c53278a91635de972b50000
Makaveli <alucardredrum@gmail.com>
2017-08-06 04:33:17 -0700

    rebased onto master

--- app/src/main/java/wyuen/kitchen_pantry/MainActivity.java
+++ app/src/main/java/wyuen/kitchen_pantry/MainActivity.java
@@ -1,12 +1,12 @@
 package wyuen.kitchen_pantry;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
